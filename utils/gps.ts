f7ed03d902818869f08ec1f9e8dc3b59f545cdc4
spectralo <alice@spectralo.me>
2025-07-01 13:56:10 +0200

    test

--- utils/gps.ts
+++ utils/gps.ts
@@ -4,16 +4,20 @@ const db = new Database("airports.db");
 
 export async function getGpsCoordinates(iata: string) {
   iata = iata.trim().toUpperCase();
+
   try {
     const airport: any = db
       .query("SELECT * FROM airport WHERE iata = ?")
       .get(iata.toUpperCase());
 
     if (!airport) {
+      console.error(`Airport with IATA code ${iata} not found`);
       return {
         exists: false,
         message: "Airport not found",
       };
+    } else {
+      console.log(`Found airport: ${airport.name} (${iata})`);
     }
 
     return {
